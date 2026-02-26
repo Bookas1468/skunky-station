555f4b83317b7646be9c5d21693e8fdaa23f5fa0
LukasCM <153393861+Bookas1468@users.noreply.github.com>
2026-02-25 19:22:50 -0600

    fix

--- Content.Shared/Weapons/Ranged/Components/GunComponent.cs
+++ Content.Shared/Weapons/Ranged/Components/GunComponent.cs
@@ -234,7 +234,7 @@ public sealed partial class GunComponent : Component
     /// The base value for how fast the projectile moves.
     /// </summary>
     [DataField]
-    public float ProjectileSpeed = 25f;
+    public float ProjectileSpeed = SharedGunSystem.projectileSpeed;
 
     /// <summary>
     /// How fast the projectile moves.
